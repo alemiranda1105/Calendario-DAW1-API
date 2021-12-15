24683cc516dd33cd14364af3d91ef20518ef9b8a
Alejandro Miranda <alejandrom@secret-source.eu>
2021-12-15 19:20:46 +0000

    Añadido comentario

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::GroupsController < ApplicationController
+  # https://stackoverflow.com/questions/36268281/rails-create-object-has-many-through <- Enlace para crear many to many
   def index
     @groups = Group.all
     respond_to :json
