048ef542aac7dd63d7fa9ff2af888d038eb77022
Alejandro Miranda <amiranda110500@gmail.com>
2021-12-16 18:23:45 +0000

    Creada semilla para inicializar la base de datos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{ username:'pepe', email:'pepe', password:'12345' }, { username:'maria', email:'maria', password:'12345' }])
+groups = Group.create([{ name:"test1" }, {name:"test2"}])
+
+for g in groups do
+    for u in users do
+        GroupUser.create(user_id: u.id, group_id: g.id)
+    end
+end
